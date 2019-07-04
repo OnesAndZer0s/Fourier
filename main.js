de9dd2419d531adb0686a79571ffed1b0b602f27
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-07-04 16:08:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,39 @@
-var canvas = document.getElementById('four');
-var ctx = canvas.getContext('2d');
+function init() {
+  window.requestAnimationFrame(draw);
+}
+
+function draw() {
+  var ctx = document.getElementById('four').getContext('2d');
+  var t = new Date().getMilliseconds();
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0, 0, 300, 300); // clear canvas
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+  ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
+  ctx.save();
+  ctx.translate(150, 150);
+
+  // Earth
+  var time = new Date();
+  ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+  ctx.translate(105, 0);
+  ctx.fillRect(0, -12, 40, 24); // Shadow
+  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+
+  // Moon
+  ctx.save();
+  ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
+  ctx.translate(0, 28.5);
+  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+  ctx.restore();
+
+  ctx.restore();
+  
+  ctx.beginPath();
+  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+  ctx.stroke();
+ 
+  window.requestAnimationFrame(draw);
+}
+
+init();
