7f74c7e70bf4cb77693a83b074cd87d79771832f
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-07-04 16:20:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,74 @@
-function init() {
-  window.requestAnimationFrame(draw);
+var frequencyInput = document.querySelector("input[frequency]");
+var orderInput = document.querySelector("input[order]");
+var waveformInput = document.getElementById("waveform").elements["waveform"];
+var canvas = document.querySelector("canvas");
+var context = canvas.getContext("2d");
+
+var TAU = Math.PI * 2.0;
+var Scale = 64.0;
+var time = 0.0;
+var startTime = new Date().getTime();
+var values = [];
+var valuePointer = 0;
+var x = 128.0,
+		y = 128.0;
+
+function fourier(order) {
+		var phase = order * time * TAU;
+		var radius = 4.0 / (order * Math.PI) * Scale;
+		context.beginPath();
+		context.lineWidth = 1.0;
+		context.strokeStyle = "rgba(255,128,32,1.0)";
+		context.arc(x, y, radius, 0, TAU);
+		context.stroke();
+		context.strokeStyle = "rgba(255,255,255,0.4)";
+		context.moveTo(x, y);
+		x += Math.cos(phase) * radius;
+		y += Math.sin(phase) * radius;
+		context.lineTo(x, y);
+		context.stroke();
+};
+
+function connect() {
+		context.beginPath();
+		context.moveTo(x + 0.5, y + 0.5);
+		context.lineTo(256 + 0.5, y + 0.5);
+		context.strokeStyle = "rgba(255,255,32,1.0)";
+		context.stroke();
+};
+
+function drawWave() {
+		values[valuePointer++ & 255] = y;
+		context.beginPath();
+		context.strokeStyle = "rgba(0,255,0,1)";
+		context.moveTo(256 + 0.5, y + 0.5);
+		for (var i = 1; i < 256; ++i) {
+				context.lineTo(256 + i + 0.5, values[(valuePointer - i) & 255] + 0.5);
+		}
+		context.stroke();
 }
 
-function draw() {
-  var ctx = document.getElementById('four').getContext('2d');
-  var t = new Date().getMilliseconds();
-  ctx.globalCompositeOperation = 'destination-over';
-  ctx.clearRect(0, 0, 300, 300); // clear canvas
-
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-  ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-  ctx.save();
-  ctx.translate(150, 150);
-
-  // Earth
-  var time = new Date();
-  ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-  ctx.translate(105, 0);
-  ctx.fillRect(0, -12, 40, 24); // Shadow
-  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-
-  // Moon
-  ctx.save();
-  ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-  ctx.translate(0, 28.5);
-  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-  ctx.restore();
-
-  ctx.restore();
-  
-  ctx.beginPath();
-  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-  ctx.stroke();
- 
-  window.requestAnimationFrame(draw);
-}
-
-init();
+(function frame() {
+		canvas.width = canvas.clientWidth;
+		canvas.height = canvas.clientHeight;
+		x = 144.0;
+		y = 128.0;
+		switch (waveformInput.value) {
+				case "square":
+						for (var order = 0; order <= orderInput.value; order++) {
+								fourier((order << 1) + 1);
+						}
+						break;
+				case "sawtooth":
+						for (var order = 1; order <= orderInput.value; order++) {
+								fourier(order << 1);
+						}
+						break;
+		}
+		connect();
+		drawWave();
+		var now = new Date().getTime();
+		time += (now - startTime) * Math.pow(10.0, frequencyInput.value);
+		startTime = now;
+		window.requestAnimationFrame(frame);
+})();
